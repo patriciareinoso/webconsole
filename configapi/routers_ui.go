e3b4e7dbdde4b201b2f4f0b6f2c7ba415e36cec4
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-06-13 15:44:18 +0000

    test slash route

--- configapi/routers_ui.go
+++ configapi/routers_ui.go
@@ -10,36 +10,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/omec-project/webconsole/backend/factory"
 	"github.com/omec-project/webconsole/backend/logger"
-	"fmt"
-	"net/http"
-	"os"
 )
 
 func AddServiceUi(engine *gin.Engine) *gin.RouterGroup {
-	group := engine.Group("/ui")
-
+	group := engine.Group("/")
 
 	routeName := factory.WebUIConfig.Configuration.Ui.RouteName
 	path := factory.WebUIConfig.Configuration.Ui.Path
 	logger.WebUILog.Infoln("AddServiceUi route %s path %s", routeName, path)
-	filename := path + "testFile.txt"
-
-	if err := os.Mkdir(path, os.ModePerm); err != nil {
-        logger.WebUILog.Infoln(err)
-    }
-	f, err := os.Create(filename)
-	
-    if err != nil {
-        logger.WebUILog.Infoln(err)
-    }
-	logger.WebUILog.Infoln("file created ", filename)
-    defer f.Close()
-	fmt.Fprintf(f, "SOMETHING")
 
 	if routeName != "" && path != "" {
 		logger.WebUILog.Infoln("Add UI service")
-		//group.Static(routeName, path)
-		group.StaticFS(routeName, http.Dir(path))
+		group.Static(routeName, path)
+		//group.StaticFS(routeName, http.Dir(path))
 	} else {
 		logger.WebUILog.Infoln("UI service is not created")
 	}
