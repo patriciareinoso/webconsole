0e609cbeab8000b053ee6d87c353ca4a5c58e00e
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-06-17 16:19:33 +0000

    use embedded files

--- configapi/routers_ui.go
+++ configapi/routers_ui.go
@@ -12,16 +12,43 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/omec-project/webconsole/backend/logger"
+	"io/fs"
+	"embed"
+	//"webconsole/ui"
 )
 
-var UI = true
+//go:embed all:dist
+var FrontendFS embed.FS
+
+func newFrontendFileServer() http.Handler {
+	frontendFS, err := fs.Sub(FrontendFS, "dist")
+	if err != nil {
+		logger.WebUILog.Fatal(err)
+	}
+	return http.FileServer(http.FS(frontendFS))
+}
  
 func AddServiceUi(engine *gin.Engine) *gin.RouterGroup {
-	group := engine.Group("/ui")
+	group := engine.Group("/")
 	logger.WebUILog.Infoln("Add UI service")
 
-	group.StaticFS("/", http.Dir("/ui"))
+	//group.StaticFS("/", http.Dir("ui"))
  
+	//frontendHandler := newFrontendFileServer()
+	//router := http.NewServeMux()
+	//router.Handle("/", frontendHandler)
+	//return nil
+
+
+	//router := gin.Default()
+	dist, err := fs.Sub(FrontendFS, "dist")
+	if err != nil {
+		logger.WebUILog.Fatal(err)
+		return nil
+	}
+   
+	group.StaticFS("/", http.FS(dist))
+	//router.Run()
 	return group
  }
 
